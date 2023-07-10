98542fda0d2de98722a36ead26cd0c7655143bca
TylizRv <caosavenger@gmail.com>
2023-07-10 22:50:25 +0100

    ProjectCreation

---
+++ RAUniversityApiBackend/DataAccess/DBUniversityContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RAUniversityApiBackend.Models.DataModels;
+
+namespace RAUniversityApiBackend.DataAccess
+{
+	public class DBUniversityContext : DbContext
+	{
+        public DBUniversityContext(DbContextOptions<DBUniversityContext> options): base(options) { }
+
+		// TODO: Add DBSets (Table of our database)
+		public DbSet<User?> User { get; set; }
+    }
+}
---
+++ RAUniversityApiBackend/Program.cs
@@ -0,0 +1,40 @@
+// 1. Using para trabajar con entity framework
+using Microsoft.EntityFrameworkCore;
+using RAUniversityApiBackend.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// 2. Connect with the SQL Server Express
+const string CONNECTION_NAME = "DBUniversity";
+
+var connectionString = builder.Configuration.GetConnectionString(CONNECTION_NAME);
+
+// 3. Add Context
+builder.Services.AddDbContext<DBUniversityContext>(
+	options => options.UseSqlServer(connectionString)
+);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
