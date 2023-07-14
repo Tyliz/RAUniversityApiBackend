ed5c40e09a04770ac261160203adeec301b0a06f
TylizRv <caosavenger@gmail.com>
2023-07-14 19:37:26 +0100

    Ejercicio de la Sesion 4 listo

---
+++ RAUniversityApiBackend/Services/StudentService.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RAUniversityApiBackend.DataAccess;
+using RAUniversityApiBackend.Models.DataModels;
+
+namespace RAUniversityApiBackend.Services
+{
+	public class StudentService
+	{
+		#region Properties
+
+		private readonly DBUniversityContext _context;
+
+		#endregion
+
+		#region Constructors
+
+		public StudentService(DBUniversityContext context)
+		{
+			_context = context;
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		public async Task<List<Student>> GetOldersThan18()
+		{
+			List<Student> students = new();
+
+			if (_context.Students != null)
+			{
+				students = await _context.Students.ToListAsync();
+
+				students = students
+					.Select(student => student)
+					.Where(student => GetAge(student.DateOfBird) >= 18)
+					.ToList();
+			}
+
+			return students;
+		}
+
+		public async Task<List<Student>> GetStudenWithAtLeastOneCourse()
+		{
+			List<Student> students = new();
+
+			if (_context.Students != null)
+			{
+				students = await _context.Students.ToListAsync();
+
+				students = students
+					.Select(student => student)
+					.Where(student => student.Courses != null && student.Courses.Any())
+					.ToList();
+			}
+
+			return students;
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// Get Age from Date of Bird
+		/// Original code from: https://stackoverflow.com/a/1404
+		/// </summary>
+		/// <returns></returns>
+		private static int GetAge(DateTime dateOfBird)
+		{
+			DateTime today = DateTime.Today;
+
+			// Calculate the age.
+			int age = today.Year - dateOfBird.Year;
+
+			// Go back to the year in which the person was born in case of a leap year
+			if (dateOfBird.Date > today.AddYears(-age)) age--;
+
+			return age;
+		}
+
+		#endregion
+	}
+}
---
+++ RAUniversityApiBackend/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RAUniversityApiBackend.DataAccess;
+using RAUniversityApiBackend.Models.DataModels;
+
+namespace RAUniversityApiBackend.Services
+{
+	public class UserService
+	{
+		#region Properties
+
+		private readonly DBUniversityContext _context;
+
+		#endregion
+
+		#region Constructors
+
+		public UserService(DBUniversityContext context)
+		{
+			_context = context;
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		public async Task<List<User>> SearchByEmail(string email)
+		{
+			List<User> users = new();
+
+			if (_context.Users != null)
+			{
+				users = await _context.Users.ToListAsync();
+
+				users = users
+					.Select(user => user)
+					.Where(x => x.Email == email)
+					.ToList();
+			}
+
+			return users;
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		#endregion
+	}
+}
